536b25b9387fce050c9259f908fe60ded1ab0f9f
Tomy20050211 <tomasruiz1104@gmail.com>
2025-08-22 21:12:42 -0500

    feat: add service filtering API, HTML, CSS, and JavaScript for service search functionality

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const app = express();
 
 // ------------------- RUTAS -------------------
 const registerRoutes = require("./routes/registers.js");
+const serviceRoutes = require("./routes/services.js");
 
 const PORT = process.env.EXPRESS_PORT || 3000;
 
@@ -16,7 +17,7 @@ app.use(express.json());
 
 // ------------------- ENDPOINTS -------------------
 app.use("/api/register", registerRoutes);
-
+app.use("/api/services", serviceRoutes);
 
 // ------------------- INICIO SERVIDOR -------------------
 app.listen(PORT, () => {
